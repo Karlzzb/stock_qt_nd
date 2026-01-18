80f1c71b18601c8d54f25237897992c9d65fc711
Karlzzb <lansedepiaomiao@gmail.com>
2026-01-18 18:46:19 +0800

    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ future_return_15d LGB:158个特征 LR: 107个特征 C=0.001 class_weight=NONE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    模型训练集表现
    ==================================================
    precision           : 0.7674
    recall              : 0.1258
    f1                  : 0.2162
    pr_auc              : 0.6212
    f1_thresh_0.3       : 0.6372
    f1_thresh_0.5       : 0.5654
    f1_thresh_0.7       : 0.2162
    precision_top6668   : 0.8884
    precision_top33343  : 0.7911
    precision_top100    : 1.0000
    
    模型测试集表现
    ==================================================
    precision           : 0.9514
    recall              : 0.0107
    f1                  : 0.0212
    pr_auc              : 0.5453
    f1_thresh_0.3       : 0.5836
    f1_thresh_0.5       : 0.1760
    f1_thresh_0.7       : 0.0212
    precision_top2213   : 0.9277
    precision_top11065  : 0.7469
    precision_top100    : 0.9200
    
    模型验证集表现
    ==================================================
    precision           : 0.9752
    recall              : 0.0280
    f1                  : 0.0545
    pr_auc              : 0.5614
    f1_thresh_0.3       : 0.6219
    f1_thresh_0.5       : 0.3006
    f1_thresh_0.7       : 0.0545
    precision_top2228   : 0.9906
    precision_top11144  : 0.7389
    precision_top100    : 1.0000

--- src/stock_model_Tflod_v2.py
+++ src/stock_model_Tflod_v2.py
@@ -293,7 +293,7 @@ def train_meta_model_reduce(X_train, y_train, oof_preds, lgb_models, config, lgb
 
     lr = LogisticRegression(
         max_iter=1000,
-        class_weight='balanced',
+        # class_weight='balanced',
         # class_weight = {0: 0.5, 1: 0.5}, # 大幅惩罚假阳性（错误预测为买入）
         random_state=config.RANDOM_STATE,
         C=0.001,  # 更强的正则化，防止过拟合
